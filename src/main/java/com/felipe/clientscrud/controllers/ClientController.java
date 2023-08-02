37825aba440edcc5a541546e3e59e9cc4fcb7a77
felipeam10 <felipeam10@hotmail.com>
2023-08-02 11:08:30 -0300

    v3.23.5 customizando resposta com response entity retornando 201 no post

--- src/main/java/com/felipe/clientscrud/controllers/ClientController.java
+++ src/main/java/com/felipe/clientscrud/controllers/ClientController.java
@@ -5,7 +5,11 @@ import com.felipe.clientscrud.services.ClientsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -14,17 +18,21 @@ public class ClientController {
     @Autowired
     private ClientsService service;
     @GetMapping(value = "/{id}")
-    public ClientsDTO findById(@PathVariable Long id) {
-        return service.findById(id);
+    public ResponseEntity<ClientsDTO> findById(@PathVariable Long id) {
+        ClientsDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ClientsDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ClientsDTO>> findAll(Pageable pageable) {
+        Page<ClientsDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping
-    public ClientsDTO insert(@RequestBody ClientsDTO dto) {
-        return service.insert(dto);
+    public ResponseEntity<ClientsDTO> insert(@RequestBody ClientsDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
